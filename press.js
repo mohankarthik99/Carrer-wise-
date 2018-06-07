a1654f802d1f05f54030c8699572c906bce95215
mohankarthik99 <32239797+mohankarthik99@users.noreply.github.com>
2018-06-07 17:39:11 +0530

    Boss corrections

--- press.js
+++ press.js
@@ -8,7 +8,6 @@ function getUserInput(){
   if (query) {
     scroll(sel("#" + query))
 }
-
 }
 
 function scroll(el){
@@ -16,4 +15,27 @@ el.scrollIntoView();
 window.scrollBy(0,-80);
 }
 getUserInput();
+
+$('.image-link').magnificPopup({
+    type: 'image',
+    mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+    zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function (openerElement) {
+            // openerElement is the element on which popup was initialized, in this case its <a> tag
+            // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+            return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+    }
+
+});
+
 });
